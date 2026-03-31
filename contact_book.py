aa69668e0a960d80d48503ae48a55d7a87f42132
Raul Shahi <rahulfromnepal2013@gmail.com>
2026-03-31 13:48:56 +0200

    Day 2 tasks completed

---
+++ contact_book.py
@@ -0,0 +1,6 @@
+contact_book = {
+    "bob":"bob@gmail.com",
+    "bib":"bib@gmail.com",
+    "chax":"chax@gmail.com",
+    "mandar":"mandar@gmail.com"
+}
---
+++ main.py
@@ -0,0 +1,15 @@
+tasks = {
+    "id":1,
+    "created_by":"Bob",
+    "assigned_to": "Buzz",
+    "created_date":"29/03/2026",
+    "description":"manage the mismatch in the id of users in the db"
+}
+
+users ={
+    "id":456,
+    "name":"Buzz",
+    "position":"developer",
+    "team":"Python",
+    "email":"buzz@gmail.com"
+}
